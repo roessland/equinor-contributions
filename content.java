78cac1bff1f38e352086006227a697e0e49593fb
Andreas Røssland <andro@equinor.com>
2017-03-29 14:27:33 +0200

    add code in files types: .html,.java removed code in files types: .html,.java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Crajvl {
     public static void main() {
         System.out.println("nbenm");
         System.out.println("lojjh");
-        System.out.println("yxfpa");
+        System.out.println("kxlvi");
     }
 }
