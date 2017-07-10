111390dc24e0c281ec4717852c7caa06d6a99912
Andreas Røssland <andro@equinor.com>
2017-07-10 10:37:36 +0200

    add code in files types: .sbt,.java,.scala removed code in files types:

--- content.java
+++ content.java
@@ -7,5 +7,6 @@ public class Cxqpva {
         System.out.println("ohdot");
         System.out.println("kwswn");
         System.out.println("kqvxk");
+        System.out.println("cmvvc");
     }
 }
