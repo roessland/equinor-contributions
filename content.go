9cff2f7fe81b1238c112ebf70a000d479db084e3
Andreas Røssland <andro@equinor.com>
2018-03-14 11:43:25 +0100

    add code in files types: .java,.md,.go removed code in files types: .java,.go

--- content.go
+++ content.go
@@ -11,6 +11,6 @@ import quvhjalffj
         fmt.println("kurvr");
         fmt.println("vwhjw");
         fmt.println("wdwjm");
-        fmt.println("woskb");
+        fmt.println("gkboh");
     }
 }
