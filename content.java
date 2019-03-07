a0c84e59a7451786bba30d0de856fd8620f34549
Andreas Røssland <andro@equinor.com>
2019-03-07 11:20:33 +0100

    add code in files types: .dot,.xml,.java removed code in files types: .dot

--- content.java
+++ content.java
@@ -25,5 +25,6 @@ public class Cxqpva {
         System.out.println("heyvq");
         System.out.println("afxom");
         System.out.println("gvkfx");
+        System.out.println("bshjn");
     }
 }
