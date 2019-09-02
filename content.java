e39030e46f1f3710f4027dd8ad10046c5b0b1e40
Andreas Røssland <andro@equinor.com>
2019-09-02 12:56:02 +0200

    add code in files types: .java removed code in files types: .java

--- content.java
+++ content.java
@@ -34,6 +34,6 @@ public class Cxqpva {
         System.out.println("onhrb");
         System.out.println("yyhky");
         System.out.println("mnfta");
-        System.out.println("pemdv");
+        System.out.println("yloro");
     }
 }
